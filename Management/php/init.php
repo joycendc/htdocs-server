5c8cf0dd16e73e26db21ca4615f97ce5898cebcf
Joycen Capili <capilijoycen0@gmail.com>
2022-05-21 17:35:55 +0800

    fix mysql error8

--- Management/php/init.php
+++ Management/php/init.php
@@ -6,5 +6,6 @@ $dbuser = 'kI4akVA6Mv';
 $dbpass = 'eQV5AziqCQ';
 $dbhost = 'remotemysql.com';
 
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 $db = new db($dbhost, $dbuser, $dbpass, $dbname);
 ?>
